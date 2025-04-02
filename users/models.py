50d622e89089348dc4c9a170268daec0d2fb73e0
ejiro osiephri <osiephriejiro765@gmail.com>
2025-04-02 17:04:32 +0100

    Feats/add setup on the way

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    username = None
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    def __str__(self):
+        return self.email
+
--- users/views.py
+++ users/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer
+from django.http import JsonResponse
 
-# Create your views here.
+User = get_user_model()
+
+
+
+def welcome_view(request):
+    return JsonResponse({"message": "Welcome to the Django API!"})
+
+# Signup
+class SignupView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Get Profile
+class ProfileView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
+
+# Logout
+class LogoutView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({"message": "Logged out successfully"}, status=200)
+        except:
+            return Response({"error": "Invalid token"}, status=400)
+
